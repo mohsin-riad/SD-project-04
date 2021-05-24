dfda530d680becb650a1e031e7df3f7c41b4a590
mohsin <morteen1997@gmail.com>
2021-05-24 21:47:57 +0600

    student/teacher: design updated

--- vuejs-project4/src/components/Student/sidebar.vue
+++ vuejs-project4/src/components/Student/sidebar.vue
@@ -5,6 +5,13 @@
                 <div class="sidebar-logo">
                     Student Panel
                 </div>
+                <div class="sidebar-logo1">
+                    <li class="header btn btn-dark btn-block"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+                        </svg> &emsp; <strong>{{ user.name }}</strong>
+                    </li>
+                </div>
                 <ul class="sidebar-navigation">
                     <li class="header">Navigation</li>
                     <li>
@@ -50,9 +57,10 @@
                     <li>
                         <router-link to="/student/results">
                             <i class="fa fa-home" aria-hidden="true"></i> 
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-checklist" viewBox="0 0 16 16">
-                                <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
-                                <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0zM7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-check" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M10.854 7.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 9.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+                                <path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+                                <path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
                             </svg>&emsp;
                             Results
                         </router-link>
@@ -111,6 +119,14 @@ export default {
         else if(role=='admin'){
             this.$router.push('/admin/dashboard');
         }
+        const baseURI = 'http://127.0.0.1:8000/api/get-user/' + token;
+        const response = await this.$http.get(baseURI, {
+            headers: {
+                Authorization: 'Bearer' + token
+            }
+        });
+        this.user = response.data.user;
+        console.log(response.data.user);
         // const baseURI = 'http://127.0.0.1:8000/api/get-user/' + token;
         // const response = await this.$http.get(baseURI);
         // console.log(response.data.user);
@@ -126,6 +142,11 @@ export default {
   padding: 10px 15px 10px 30px;
   font-size: 20px;
   background-color: #47575f;
+}
+.sidebar-logo1 {
+  padding: 10px 15px 10px 30px;
+  font-size: 20px;
+  background-color: #131212;
 }
 .sidebar-navigation li::before {
   background-color: #363636;
